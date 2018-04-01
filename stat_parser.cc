bf0b73c060758c565b0d31019aa099a5b44c1893
JuanitoAli <jcflorezv@unal.edu.co>
2018-04-01 15:02:49 -0500

    token size checker ready...

--- stat_parser.cc
+++ stat_parser.cc
@@ -16,43 +16,17 @@ int main(int argc, char **argv) {
     vector<string> rare_lines;
     bool rare_flags[6] = {};
     
-    unsigned i = 0;
+    unsigned i = 1;
     while(getline(input, log_line)) {
         tokens = split(log_line);
-        sz = tokens.size();
-        switch (sz):
-        case 10: 
-            if(!rare_flags[0])
-                rare_lines.push_back(log_line);
-            rare_flags[0] = true;
+        if(tokens.size() != 10) {
+            cout << i << endl;
+            cout << tokens.size() << endl;
+            cout << "exit" << endl;
             break;
-        case 11: 
-            if(!rare_flags[1])
-                rare_lines.push_back(log_line);
-            rare_flags[1] = true;
-            break;
-        case 12: 
-            if(!rare_flags[2])
-                rare_lines.push_back(log_line);
-            rare_flags[2] = true;
-            break;
-        case 13: 
-            if(!rare_flags[3])
-                rare_lines.push_back(log_line);
-            rare_flags[3] = true;
-            break;
-        case 14: 
-            if(!rare_flags[4])
-                rare_lines.push_back(log_line);
-            rare_flags[4] = true;
-            break;
-        case 15: 
-            if(!rare_flags[5])
-                rare_lines.push_back(log_line);
-            rare_flags[5] = true;
-            break;
+        }
+        i++;
     }
-
 }
 
 vector<string> split(string line) {
@@ -60,17 +34,13 @@ vector<string> split(string line) {
     string buf = "";
     for(size_t i = 0; i < line.length(); i++) {
         if(line[i] == ' ') {
-            tokens.push_back(buf);
+            if(buf != "")
+                tokens.push_back(buf);
             buf = "";
             continue;
         }
         buf += line[i];
     }
     tokens.push_back(buf);       // last chars are not saved because never occurs a blank character...
-
-    for ( int i = 0; i < tokens.size(); i++)
-        cout << tokens[i] << "_";
-    cout << endl;
-    cout << "prev size(): " << tokens.size() << endl;
-return tokens;
+    return tokens;
 }
